ae27bd9d49e2047d2f0c7dfc6fe4dd9c0f37a837
Layne Faler <laynefaler@gmail.com>
2019-02-24 12:53:13 -0800

    Updates to railspp

--- lib/railspp.rb
+++ lib/railspp.rb
@@ -1,3 +1,10 @@
+require_relative './commands/initialize.rb'
+require_relative './commands/make_test.rb'
+require_relative './commands/model.rb'
+require_relative './help/documentation.rb'
+require_relative './help/initialize.rb'
+require_relative './help/make_test.rb'
+require_relative './help/model.rb'
 
 
 class RailsPlusPlus
