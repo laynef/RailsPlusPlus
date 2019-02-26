ae9c43bfc95a952e4ba21b0dd7e07f4fc87fa873
Layne Faler <laynefaler@gmail.com>
2019-02-26 11:19:43 -0800

    Working on unit tests

--- lib/commands/model.rb
+++ lib/commands/model.rb
@@ -19,12 +19,8 @@ class ModelCommand < MoreUtils
             system("rails generate model #{model_name} #{others.join(' ')}")
 
             api_version_path = lookup[:"api-version"] || 'api/v1'
+
             system("mkdir -p #{root}/app/controllers/#{api_version_path}")
-
-            if lookup[:"with-mini-test"]
-                system("rails generate scaffold #{api_version_path}/#{model_name.downcase} #{others.join(' ')}")
-            end
-
             controller_prefix = api_version_path.split('/').map { |e| e.downcase.capitalize }.join('::')
             controller_name = controller_prefix + '::' + model_name
 
@@ -32,6 +28,20 @@ class ModelCommand < MoreUtils
             controller_regex = '{{ CONTROLLER_NAME }}'
             controller_str = controller_temp.gsub(controller_regex, controller_name)
             write_file("#{root}/app/controllers/#{api_version_path}/#{model_name.underscore}_controller.rb", controller_str)
+
+            if lookup.has_key?(:"with-mini-test")
+                system("mkdir -p #{root}/test/controllers/#{api_version_path}")
+                mini_test_temp =  get_file_str("#{this_dir}/../templates/mini_test_controller.txt")
+                test_regex = '{{ NAMESPACE }}'
+                params_regex = '{{ PARAMS }}'
+                snake_case_regex = '{{ SNAKE_CASE_NAMESPACE }}'
+                snake_case_controller = controller_name.split('::').join('_').underscore
+                params_str = "{ #{others.map { |e| "#{e}: " }} }"
+                test_str = mini_test_temp.gsub(test_regex, controller_name)
+                test_str = test_str.gsub(params_regex, controller_name)
+                test_str = test_str.gsub(snake_case_regex, snake_case_controller)
+                write_file("#{root}/test/controllers/#{api_version_path}/#{model_name.underscore}_controller_test.rb", test_str)
+            end
 
             puts "#{model_name} model, migration, and controller has been generated."
         end
