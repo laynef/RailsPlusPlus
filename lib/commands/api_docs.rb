4e824b579fa1c066031d807571cb739c686bcff4
Layne Faler <laynefaler@gmail.com>
2019-03-13 17:16:43 -0700

    Changing initialize to separation documentations as it's own command

---
+++ lib/commands/api_docs.rb
@@ -0,0 +1,41 @@
+require_relative '../utils/strings.rb'
+
+
+class ApiDocsCommand < MoreUtils
+  class << self
+
+    def run(*args)
+      lookup = flag_lookup(args)
+
+      # Add Initializers
+      adi_template = get_file_str("#{this_dir}/../templates/api_documentation_initializer.txt")
+      write_file("#{root}/config/initializers/api_documentation_js.rb", adi_template)
+
+      # Add Controllers
+      dc_template = get_file_str("#{this_dir}/../templates/documentation_controller.txt")
+      write_file("#{root}/app/controllers/documentation_controller.rb", dc_template)
+
+      # Add Service
+      system("mkdir -p #{root}/app/services")
+      aps_template = get_file_str("#{this_dir}/../templates/api_documentation_service.txt")
+      write_file("#{root}/app/services/api_documentation_service.rb", aps_template)
+
+      # Add Views
+      system("mkdir -p #{root}/app/views/documentation")
+      dihtml_template = get_file_str("#{this_dir}/../templates/documentation.index.erb.txt")
+      write_file("#{root}/app/views/documentation/index.html.erb", dihtml_template)
+
+      dlhtml_template = get_file_str("#{this_dir}/../templates/documentation.layout.erb.txt")
+      write_file("#{root}/app/views/layouts/documentation.html.erb", dlhtml_template)
+
+      # Add Routes
+      routes_template = get_file_str("#{this_dir}/../templates/routes_documentation.txt")
+      routes_file = get_file_str("#{root}/config/routes.rb")
+      new_routes = routes_arr.slice(0, last_end_line).join("\n") + "\n#{routes_template}\nend\n"
+      write_file("#{root}/config/routes.rb", new_routes)
+      
+      puts "Added Automatic API documentation to your project."
+    end
+
+  end
+end
---
+++ lib/help/api_docs.rb
@@ -0,0 +1,6 @@
+require_relative '../utils/strings.rb'
+
+
+class ApiDocsHelpCommand < MoreUtils
+
+end
